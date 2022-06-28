901058e506a54acb1adcaf546a25e02ff009cf1f
BraeTroutman <btroutma@redhat.com>
2022-06-28 16:59:49 -0400

    remove cat from run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,3 @@ source /usr/src/io/conf.env
 echo "++++++++++++++++HELLO WORLD++++++++++++++++++"
 
 obslytics export --match=$MATCH --resolution=$RESOLUTION --min-time=$MIN_TIME --max-time=$MAX_TIME --input-config-file=/usr/src/io/input-config.yaml --output-config-file=/usr/src/io/output-config.yaml
-
-cat /usr/src/io/out.pq
