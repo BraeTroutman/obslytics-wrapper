2d5ec4aa47e223d146df4be927442333dc4c8c48
BraeTroutman <braetroutman@gmail.com>
2022-07-05 11:20:15 -0400

    Add loop functionality to run.sh
    Add ability to loop through the contents of conf.env to run.sh in order to allow for multiple queries to be run with every deployment of the obslytics-wrapper image

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-source /usr/src/io/conf.env
+while read line
+do
+  if [[ -z $line ]]
+  then
+    out=$(eval "cat <<EOF
+$(</usr/src/io/output-config.yaml)
+    " 2> /dev/null)
+    echo "$out"
+    obslytics export --match=$MATCH --resolution=$RESOLUTION --min-time=$MIN_TIME --max-time=$MAX_TIME --input-config-file=/usr/src/io/input-config.yaml --output-config="$out"
+  else
+    eval $line
+  fi
+done < /usr/src/io/conf.env
 
-echo "++++++++++++++++HELLO WORLD++++++++++++++++++"
-
-obslytics export --match=$MATCH --resolution=$RESOLUTION --min-time=$MIN_TIME --max-time=$MAX_TIME --input-config-file=/usr/src/io/input-config.yaml --output-config-file=/usr/src/io/output-config.yaml
