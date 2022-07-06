57a874aef6813f4b54a49383120384765147ab86
BraeTroutman <braetroutman@gmail.com>
2022-07-06 11:04:09 -0400

    Remove echo of AWS creds
    Superfluous printing of AWS creds to confirm proper functioning is no
    longer needed for debugging

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ do
     out=$(eval "cat <<EOF
 $(</usr/src/io/output-config.yaml)
     " 2> /dev/null)
-    echo "$out"
     obslytics export --match=$MATCH --resolution=$RESOLUTION --min-time=$MIN_TIME --max-time=$MAX_TIME --input-config-file=/usr/src/io/input-config.yaml --output-config="$out"
   else
     eval $line
