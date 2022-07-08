0374e44508ceb13b069022e4cb8d14cb6193a28a
BraeTroutman <btroutma@redhat.com>
2022-07-08 10:53:05 -0400

    Add outer while loop to repeatedly execute
    repeat queries every ten minutes for sliding window of cluster metrics

--- run.sh
+++ run.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-while read line
+while true
 do
-  if [[ -z $line ]]
-  then
-    out=$(eval "cat <<EOF
+  while read line
+  do
+    if [[ -z $line ]]
+    then
+      out=$(eval "cat <<EOF
 $(</usr/src/io/output-config.yaml)
-    " 2> /dev/null)
-    obslytics export --match=$MATCH --resolution=$RESOLUTION --min-time=$MIN_TIME --max-time=$MAX_TIME --input-config-file=/usr/src/io/input-config.yaml --output-config="$out"
-  else
-    eval $line
-  fi
-done < /usr/src/io/conf.env
+      " 2> /dev/null)
+      obslytics export --match=$MATCH --resolution=$RESOLUTION --min-time=$MIN_TIME --max-time=$MAX_TIME --input-config-file=/usr/src/io/input-config.yaml --output-config="$out"
+    else
+      eval $line
+    fi
+  done < /usr/src/io/conf.env
+  echo "sleeping until: $(date --date="10 minutes")"
+  sleep 600
+done
 
