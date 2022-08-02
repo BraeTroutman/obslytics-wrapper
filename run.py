13dd6f5107dd7a43db0d85b5ed0e089de982ef58
BraeTroutman <btroutma@redhat.com>
2022-08-02 12:45:22 -0400

    Add comments
    to clarify intentions and actions performed by code

--- run.py
+++ run.py
@@ -10,6 +10,10 @@ import time
 import os
 import io
 
+## GLOBALS
+
+# Internal data structure corresponding
+# to the content of query.yaml file
 QUERY_SCHEMA = Map({
     "resolution": Str(),
     "min_time": Str(),
@@ -18,6 +22,8 @@ QUERY_SCHEMA = Map({
     "matches": Seq(Str())
     })
 
+# Internal data structure corresponding
+# to the content of output-config.yaml file
 OUTPUT_CONFIG_SCHEMA = Map({
     "type": Str(),
     "path": Str(),
@@ -32,7 +38,9 @@ OUTPUT_CONFIG_SCHEMA = Map({
         })
     })
 
+# Holds parsed contents of query.yaml
 query = {}
+# Holds parsed contents of output-config.yaml
 output_config = ""
 
 while True:
@@ -44,6 +52,8 @@ while True:
 
     min_time = dateparser.parse(query['min_time'].data).astimezone().isoformat()
     max_time = dateparser.parse(query['max_time'].data).astimezone().isoformat()
+    
+    # run the obslytics tool with current params for every match listed in config
     for match in query['matches']:
         output_config['path'] = match.data.replace(':', '->') + '.pq'
         sub.run('obslytics export' \
@@ -55,8 +65,12 @@ while True:
                 f' --output-config="{output_config.as_yaml()}"'
                 , shell=True
                 , capture_output=True)
+
+    # open s3 bucket for io
     s3_resource = boto3.resource('s3', aws_access_key_id=os.environ['ACCESS'], aws_secret_access_key=os.environ['SECRET'])
     bucket = s3_resource.Bucket(output_config['storage']['config']['bucket'].data)
+
+    # list of keys in bucket, metric names, and columns to drop from each df
     key_list = [obj.key for obj in filter(lambda x: x.key != 'metrics.pq', bucket.objects.all())]
     metric_list = [item for item in map(lambda s: s.removesuffix('.pq'), key_list)]
     drop_cols = ['_sample_end', '_min_time', '_max_time', '_count', '_min', '_max']
@@ -75,14 +89,16 @@ while True:
            else:
                agg_table = pd.merge_asof(agg_table, table.sort_values(by=['timestamp']), on='timestamp', by='cluster', tolerance=pd.Timedelta('5s'))
            i += 1
+
+    # remove duplicate columns from final df
     dup_cols_x = [item for item in filter(lambda x: '_x' in x, agg_table.columns)]
     cols = [item for item in map(lambda x: x.removesuffix('_x'), dup_cols_x)]
     dup_cols_y = [item for item in map(lambda x: x + '_y', cols)]
     x_to_norm = {x: norm for (x, norm) in zip(dup_cols_x, cols)}
-    
     agg_table = agg_table.rename(columns=x_to_norm).drop(columns=dup_cols_y)
     agg_table = agg_table.loc[:,~agg_table.columns.duplicated()].copy()
-    
+   
+    # write to s3
     with io.BytesIO() as f:
         agg_table.to_parquet(f)
         f.seek(0)
